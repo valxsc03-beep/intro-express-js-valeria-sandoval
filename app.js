1bdb96fba24e67c719629bacb87ea366f38e9cf3
valxsc03-beep <valxsc03@gmail.com>
2026-07-30 13:44:48 -0500

    enpoint lista productos

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 import express from 'express';
-import {configDotenv} from "dotenv"
-configDotenv();
+// import {configDotenv} from "dotenv"
+// configDotenv();
+import "dotenv/config" 
+import bodyparser from "body-parser";
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
+
+//configurar el uso de body-parser
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 
 app.get("/", (_, res) => {
-  res.send('Hola , estamos aprendiendo express con la ficha 3407184');
+  res.send(`Hola , estamos aprendiendo express con la ficha 3407184`);
+});
+
+//otro endopint, funcione de flecha
+app.get("/productos", (req,res) => {
+  //usando templates strings
+  res.send(`<h1>listado de productos</h1>
+    <ol>
+      <li>televisor</li>
+      <li>celular</li>
+      <li>impresora</li>
+    </ol>`);
 });
 
 app.listen(port, () => {
