739c630dd3c1bfd789a12ea65accd50fa5fe32a2
williamestupinansilva-oss <valxsc03@gmail.com>
2026-08-06 16:19:18 -0500

    cambios en el app en la rama commonjs

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-import express from 'express';
+require("dotenv").config();
+
+const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (_, res) => {
   res.send('Hola , estamos aprendiendo express con la ficha 3407184');
 });
+
+app.get('/ruta1', (_, res) => {
+  res.json({
+    datos_personales: {
+      nombre: "Valeria",
+      apellido: "Sandoval",
+      lista_telefonos: ["3112557707", "3203704157"] 
+    },
+    datos_programa: {
+      nombre: "ADSO",
+      tipo: "Tecnologo",
+      ficha: 3407184
+    }
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto: ${port}`);
