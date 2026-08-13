57ebdbf283d7052bf9ea1655cacd8a6e87cf3fcf
valxsc03-beep <valxsc03@gmail.com>
2026-08-13 15:27:21 -0500

    rutas dinamicas

--- app.js
+++ app.js
@@ -6,26 +6,46 @@ app.get("/", function(req, res) {
   res.send("API de aprendices, Enpoint principal");
 });
 
+
+const aprendices = [
+  {
+    nombre: "valeria",
+    edad: 18,
+    correo: "valxsc03@gmail.com",
+  },
+  {
+    nombre: "eileen",
+    edad: 17,
+    correo: "eileen@gmail.com",
+  },
+  {
+    nombre: "joshua",
+    edad: 18,
+    correo: "joshua@gmail.com",
+  },
+];
+
 app.get("/aprendices", (req, res) => {
-  const aprendices = [
-    {
-      "Nombre": "Valeria",
-      "Edad": 18,
-      "Correo": "valxsc03@gmail.com",
-    },
-    {
-      "Nombre": "Eileen",
-      "Edad": 17,
-      "Correo": "eileen@gmail.com",
-    },
-    {
-      "Nombre": "Joshua",
-      "Edad": 18,
-      "Correo": "joshua@gmail.com",
-    },
-  ];
   res.json(aprendices);
 });
+
+app.get("/aprendices/nombre/:nombre", (req, res) => {
+  const { nombre } = req.params;
+
+  const aprendizEncontrado = aprendices.find(
+    (aprendiz) => aprendiz.nombre.toLowerCase() === nombre.toLowerCase()
+  );
+
+  if (aprendizEncontrado) {
+    res.json(aprendizEncontrado);
+  } else {
+  
+    res.status(404).json({ mensaje: "Aprendiz no encontrado" });
+  }
+});
+
+
+
 
 
 app.listen(port, function() {
